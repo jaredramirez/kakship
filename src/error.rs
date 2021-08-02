be88c688b6e0b1ff3870dcda8afbb9cf2f3a32ce
Jared Ramirez <jaredramirez@hey.com>
2021-08-02 13:19:53 -0700

    Fix bug

--- src/error.rs
+++ src/error.rs
@@ -1,35 +1,34 @@
 use std::{env, fmt, io};
 
-
 pub enum Error {
-	ConfigVarError(env::VarError),
-	IoError(io::Error),
-	StarshipError(String),
+    ConfigVarError(env::VarError),
+    IoError(io::Error),
+    StarshipError(String),
 }
 
 impl fmt::Debug for Error {
-	fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
-		match *self {
+    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
+        match *self {
 			Error::ConfigVarError(ref err) => {
 				match err {
-					env::VarError::NotPresent => write!(f, "$kak_config should be exported and point to a directory containing starship.toml"),
-					env::VarError::NotUnicode(_) => write!(f, "$kak_config value is not valid")
+					env::VarError::NotPresent => write!(f, "$kak_config or $kak_runtime should be exported and point to a directory containing starship.toml"),
+					env::VarError::NotUnicode(_) => write!(f, "$kak_config or $kak_runtime value is not valid")
 				}
 			},
 			Error::IoError(ref err) => write!(f, "Error executing starship {:?}", err),
 			Error::StarshipError(ref err) => write!(f, "{:?}", err)
 		}
-	}
+    }
 }
 
 impl From<env::VarError> for Error {
-	fn from(err: env::VarError) -> Self {
-		Error::ConfigVarError(err)
-	}
+    fn from(err: env::VarError) -> Self {
+        Error::ConfigVarError(err)
+    }
 }
 
 impl From<io::Error> for Error {
-	fn from(err: io::Error) -> Self {
-		Error::IoError(err)
-	}
+    fn from(err: io::Error) -> Self {
+        Error::IoError(err)
+    }
 }
--- src/main.rs
+++ src/main.rs
@@ -5,18 +5,21 @@ use kak::{escape::Mode, face};
 use std::{env, path::Path, process::Command};
 
 fn main() -> Result<(), Error> {
-    let config_dir = env::var("kak_config")?;
+    let opt_config_dir = env::var("kak_config");
+    let opt_runtime_dir = env::var("kak_runtime");
+
+    let config_dir = match (opt_config_dir, opt_runtime_dir) {
+        (Ok(dir), _) => Ok(dir),
+        (_, Ok(dir)) => Ok(dir),
+        _ => Err(Error::ConfigVarError(env::VarError::NotPresent)),
+    }?;
+
     let config = Path::new(&config_dir).join("starship.toml");
-
-    let runtime_dir = env::var("kak_runtime")?;
-    let runtime = Path::new(&runtime_dir).join("starship.toml");
-
-    let starship_config = if config.exists() { config } else { runtime };
 
     let args: Vec<String> = env::args().skip(1).collect();
     let starship = Command::new("starship")
         .env("STARSHIP_SHELL", "sh")
-        .env("STARSHIP_CONFIG", starship_config)
+        .env("STARSHIP_CONFIG", config)
         .args(&args)
         .output()?;
 
