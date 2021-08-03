c6d39b73be7f7c13f37709af3dfec9df10da6205
Jared Ramirez <jaredramirez@hey.com>
2021-08-03 12:25:36 -0700

    Check runtime dir first

--- src/main.rs
+++ src/main.rs
@@ -2,19 +2,31 @@ mod error;
 
 use crate::error::Error;
 use kak::{escape::Mode, face};
-use std::{env, path::Path, process::Command};
+use std::{env, env::VarError, path::Path, path::PathBuf, process::Command};
+
+const CONFIG_FILE_NAME: &str = "starship.toml";
+
+fn get_config_from_env(env_var: &str) -> Result<PathBuf, VarError> {
+    env::var(env_var).and_then(|dir| {
+        let path = Path::new(&dir).join(CONFIG_FILE_NAME);
+        println!("{}: {:?} {}", env_var, path, path.exists());
+        if path.exists() {
+            Ok(path)
+        } else {
+            Err(env::VarError::NotPresent)
+        }
+    })
+}
 
 fn main() -> Result<(), Error> {
-    let opt_config_dir = env::var("kak_config");
-    let opt_runtime_dir = env::var("kak_runtime");
+    let res_runtime_config = get_config_from_env("kak_runtime");
+    let res_config_config = get_config_from_env("kak_config");
 
-    let config_dir = match (opt_config_dir, opt_runtime_dir) {
-        (Ok(dir), _) => Ok(dir),
-        (_, Ok(dir)) => Ok(dir),
+    let config = match (res_runtime_config, res_config_config) {
+        (Ok(path), _) => Ok(path),
+        (_, Ok(path)) => Ok(path),
         _ => Err(Error::ConfigVarError(env::VarError::NotPresent)),
     }?;
-
-    let config = Path::new(&config_dir).join("starship.toml");
 
     let args: Vec<String> = env::args().skip(1).collect();
     let starship = Command::new("starship")
